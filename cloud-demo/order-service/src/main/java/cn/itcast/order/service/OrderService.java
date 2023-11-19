9f23cbf21d522c628ca153a2e51f0590004c843d
一路 <148515341+yi-lu-o@users.noreply.github.com>
2023-11-19 21:18:33 +0800

    学习了Foreign远程调用和Gateway网关服务

--- cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package cn.itcast.order.service;
 
+import cn.itcast.order.client.UserClient;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.pojo.User;
@@ -12,15 +13,18 @@ public class OrderService {
 
     @Autowired
     private OrderMapper orderMapper;
-    @Autowired
-    private RestTemplate restTemplate;
+//    @Autowired
+//    private RestTemplate restTemplate;
+    private UserClient userClient;
+
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
         // 2.远程查询user
         //String url = "http://localhost:8081/user/"+order.getUserId();
         String url = "http://userservice/user/"+order.getUserId();
-        User user = restTemplate.getForObject(url, User.class);
+        //User user = restTemplate.getForObject(url, User.class);
+        User user = userClient.findById(order.getUserId());
         // 3.存入order
         order.setUser(user);
         // 4.返回
